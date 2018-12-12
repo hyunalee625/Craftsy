13b3bc00cf85e6548181d6f7c3c1bae6209e1d20
Peter Zeng <pzeng.pzeng@gmail.com>
2018-12-11 17:25:46 -0800

    Create App and Root components. Import Root component into entry file to be used in ReactDOM.render function

--- frontend/craftsy.jsx
+++ frontend/craftsy.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as SessionAPIUtil from "./util/session_api_util";
 import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
@@ -12,5 +12,5 @@ document.addEventListener("DOMContentLoaded", () => {
   window.dispatch = store.dispatch;
   //end testing
 
-  ReactDOM.render(<h1>Welcome to Craftsy!</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
