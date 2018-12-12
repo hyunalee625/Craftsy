eb8938f02543bf0e31b84c73fd59dd7a8ca5d4e3
Peter Zeng <pzeng.pzeng@gmail.com>
2018-12-11 16:26:43 -0800

    Implement session_actions

--- frontend/craftsy.jsx
+++ frontend/craftsy.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as SessionAPIUtil from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
+  //for testing
+  window.signup = SessionAPIUtil.signup;
+  window.login = SessionAPIUtil.login;
+  window.logout = SessionAPIUtil.logout;
+  //end testing
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to Craftsy!</h1>, root);
 });
