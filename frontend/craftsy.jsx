40313938cb54cf40495bb0da4de55c348d0a74dd
Peter Zeng <pzeng.pzeng@gmail.com>
2018-12-11 17:08:36 -0800

    Update entry file to import configureStore

--- frontend/craftsy.jsx
+++ frontend/craftsy.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as SessionAPIUtil from "./util/session_api_util";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
-  //for testing
-  window.signup = SessionAPIUtil.signup;
-  window.login = SessionAPIUtil.login;
-  window.logout = SessionAPIUtil.logout;
-  //end testing
+  const store = configureStore();
   const root = document.getElementById("root");
+
+  //for testing
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  //end testing
+
   ReactDOM.render(<h1>Welcome to Craftsy!</h1>, root);
 });
