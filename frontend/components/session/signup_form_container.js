69267a8ac61e91570ea07dccac249c33ace38027
Peter Zeng <pzeng.pzeng@gmail.com>
2018-12-12 07:08:26 -0800

    Add loginFormContainer, signupFormContainer, and sessionForm

---
+++ frontend/components/session/signup_form_container.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { signup } from "../../actions/session_actions";
+import SessionForm from "./session_form";
+
+const mapStateToProps = (state, ownProps) => ({
+  errors: state.errors.session,
+  formType: "Sign Up",
+  navLink: <Link to="/login">Log In</Link>
+});
+
+const mapDispatchToProps = dispatch => ({
+  processForm: user => dispatch(signup(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SessionForm);
