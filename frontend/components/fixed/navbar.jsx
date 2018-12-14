e6d9fe9197ea72829a6d51bba501d0c12ff0a415
Peter Zeng <pzeng.pzeng@gmail.com>
2018-12-13 16:22:39 -0800

    Complete user auth styling

--- frontend/components/fixed/navbar.jsx
+++ frontend/components/fixed/navbar.jsx
@@ -7,6 +7,7 @@ class NavBar extends React.Component {
     this.sessionLinks = this.sessionLinks.bind(this);
     this.loggedInGreeting = this.loggedInGreeting.bind(this);
     this.greeting = this.greeting.bind(this);
+    this.dropDown = this.dropDown.bind(this);
   }
 
   sessionLinks() {
@@ -34,16 +35,37 @@ class NavBar extends React.Component {
     );
   }
 
+  dropDown() {
+    document.getElementById("user-dropdown-container").classList.toggle("show");
+    console.log("hey");
+  }
+
   loggedInGreeting() {
     return (
-      <div className="greeting-container">
-        <div className="welcome-message-container">
-          <h3 className="welcome">Hey,</h3>
-          <h3 className="username">{this.props.currentUser.username}!</h3>
-        </div>
-        <button className="login-logout-button" onClick={this.props.logout}>
-          <div className="logout-button-text">Sign Out</div>
+      <div className="entire-user-dropdown">
+        <button className="clickable-user-button" onClick={this.dropDown}>
+          <img
+            className="profile-pic"
+            src={window.defaultProfilePicURL}
+            alt="default profile pic"
+          />
+          <div className="YOU-container">
+            <h3 className="YOU-text">You</h3>
+            <h3 className="YOU-triangle">&#9660;</h3>
+          </div>
         </button>
+
+        <div id="user-dropdown-container" className="user-dropdown-content">
+          <ul>
+            <li>Favorites (Coming soon...)</li>
+            <li>Conversations (Coming soon...)</li>
+            <li>Purchases and reviews (Coming soon...)</li>
+            <li>Account settings (Coming soon...)</li>
+            <li className="logout-button-text" onClick={this.props.logout}>
+              Sign Out
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
@@ -69,5 +91,24 @@ class NavBar extends React.Component {
     );
   }
 }
+
+window.onclick = function(event) {
+  if (
+    !event.target.matches(".clickable-user-button") &&
+    !event.target.matches(".profile-pic") &&
+    !event.target.matches(".YOU-container") &&
+    !event.target.matches(".YOU-text") &&
+    !event.target.matches(".YOU-triangle")
+  ) {
+    let dropdowns = document.getElementsByClassName("user-dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
 
 export default NavBar;
