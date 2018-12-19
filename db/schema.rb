bd22003c862c6a98265922f35cdf5912de116f17
Peter Zeng <pzeng.pzeng@gmail.com>
2018-12-18 20:32:09 -0800

    Add migration and model  for shopping cart items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_011950) do
+ActiveRecord::Schema.define(version: 2018_12_19_042712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(version: 2018_12_14_011950) do
     t.index ["product_name"], name: "index_products_on_product_name"
     t.index ["user_id", "product_name"], name: "index_products_on_user_id_and_product_name", unique: true
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "shopping_cart_items", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "product_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_shopping_cart_items_on_product_id"
+    t.index ["user_id"], name: "index_shopping_cart_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
