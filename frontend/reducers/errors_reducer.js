9384da102abe2fad3dc4fb8ba518086b30f26637
Peter Zeng <pzeng.pzeng@gmail.com>
2018-12-31 20:46:08 -0800

    Add reviews reducer and review errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import sessionErrorsReducer from "./session_errors_reducer";
 import productErrorsReducer from "./product_errors_reducer";
 import quantityErrorReducer from "./quantity_error_reducer";
+import reviewErrorsReducer from "./review_errors_reducer";
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   product: productErrorsReducer,
-  quantity: quantityErrorReducer
+  quantity: quantityErrorReducer,
+  review: reviewErrorsReducer
 });
 
 export default errorsReducer;
