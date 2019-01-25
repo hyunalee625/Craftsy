1f0e6a693a08084cabbe8f7d748fa7306ba2cbb8
Peter Zeng <pzeng.pzeng@gmail.com>
2019-01-25 09:53:53 -0800

    Add custom error message for rating validation failure

--- app/models/review.rb
+++ app/models/review.rb
@@ -14,7 +14,7 @@
 class Review < ApplicationRecord
   validates :body, :rating, :user_id, :product_id, presence: true
   validates :body, length: {maximum: 300}
-  validates :rating, inclusion: {in: (1..5)}
+  validates :rating, inclusion: {in: (1..5), :message => "must be between 1 and 5"}
   validates :product_id, uniqueness: { scope: :user_id }
 
   belongs_to :user
